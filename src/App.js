5f4940333306b05f593ec060de598ac86a0e453a
anuruddhhit07 <anuruddh07@hanyang.ac.kr>
2023-02-22 11:14:41 +0900

    myfirst

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import logo from "./logo.svg";
+import "./styles.scss";
+import Navigation from "./Component/Header/Navbar";
+import Gallery from "./Component/DummyPage/Gallery";
+import { Routes, Route } from "react-router-dom";
+
+
+function App() {
+  return (
+    <div className="container">
+      <div className="header">
+        <Navigation />
+      </div>
+
+      {/* <div className="height-100"> */}
+      <div className="mainbody">
+      <Routes>
+        <Route exact path="/" element={<Gallery />} />
+        {/* <Route exact path="/svgimg" element={<MyVisComponent />} />
+         <Route path="/contact" element={<Barchart />} />
+         <Route path="/images" element ={<Linechart/>}/> */}
+         {/* <Route path="/canvasimg"element={<CanvaS/>}/>
+         <Route path="/svgcanvas" element={<SVGCS/>}/>
+         <Route path="/svgcd" element={<MySvgCD/>}/>
+         <Route path="/cwrapper" element={<D3Chart/>}/>
+         <Route path="/app2" element={<EnterApi/>}/>
+         <Route path="/ex" element={<Maincall />}/> */}
+      
+      </Routes>
+      {/* </div> */}
+      </div>
+      <div className="footer">Footer</div>
+    </div>
+  );
+}
+
+export default App;
