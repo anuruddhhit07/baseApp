e687cdb0b19f7004aeb15ef58a357bce9434b688
anuruddhhit07 <anuruddh07@hanyang.ac.kr>
2023-02-22 17:36:07 +0900

    1

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import "./styles.scss";
 import Navigation from "./Component/Header/Navbar";
 import Gallery from "./Component/DummyPage/Gallery";
 import { Routes, Route } from "react-router-dom";
-
+import EnterApi from "./Component/App/EnterApi";
+import ZoomieCircle from "./Component/App/Zoom/Enterzoom";
 
 function App() {
   return (
@@ -16,9 +17,9 @@ function App() {
       <div className="mainbody">
       <Routes>
         <Route exact path="/" element={<Gallery />} />
-        {/* <Route exact path="/svgimg" element={<MyVisComponent />} />
-         <Route path="/contact" element={<Barchart />} />
-         <Route path="/images" element ={<Linechart/>}/> */}
+        <Route exact path="/about" element={<EnterApi />} />
+         <Route path="/images" element={<ZoomieCircle />} />
+         {/* <Route path="/images" element ={<Linechart/>}/> */}
          {/* <Route path="/canvasimg"element={<CanvaS/>}/>
          <Route path="/svgcanvas" element={<SVGCS/>}/>
          <Route path="/svgcd" element={<MySvgCD/>}/>
